66586ac17a7dca68768e7917f9eb31f5b9b918ec
dmx <dingmingxin20@gmail.com>
2017-08-07 17:27:50 +0800

    add int64 type check and support

--- protobuf/protobuf.lua
+++ protobuf/protobuf.lua
@@ -138,9 +138,9 @@ local NON_PACKABLE_TYPES = {
 
 local _VALUE_CHECKERS = {
     [FieldDescriptor.CPPTYPE_INT32] = type_checkers.Int32ValueChecker(),
-    [FieldDescriptor.CPPTYPE_INT64] = type_checkers.Int32ValueChecker(),
+    [FieldDescriptor.CPPTYPE_INT64] = type_checkers.Int64ValueChecker(),
     [FieldDescriptor.CPPTYPE_UINT32] = type_checkers.Uint32ValueChecker(),
-    [FieldDescriptor.CPPTYPE_UINT64] = type_checkers.Uint32ValueChecker(),
+    [FieldDescriptor.CPPTYPE_UINT64] = type_checkers.Uint64ValueChecker(),
     [FieldDescriptor.CPPTYPE_DOUBLE] = type_checkers.TypeChecker({number = true}),
     [FieldDescriptor.CPPTYPE_FLOAT] = type_checkers.TypeChecker({number = true}),
     [FieldDescriptor.CPPTYPE_BOOL] = type_checkers.TypeChecker({boolean = true, bool = true, int=true}),
@@ -313,6 +313,7 @@ local function _AttachFieldHelpers(message_meta, field_descriptor)
         AddDecoder(wire_format.WIRETYPE_LENGTH_DELIMITED, True)
     end
 end
+
 
 local function _AddEnumValues(descriptor, message_meta)
     for _, enum_type in ipairs(descriptor.enum_types) do
